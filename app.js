f6dd82b3c2e2a7c6dc41bf5a6a393efea3b594a1
mahibulmasud <mahibulmasud@gmail.com>
2021-02-15 02:09:17 +0600

    edited

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ const createSlider = () => {
   document.querySelector('.main').style.display = 'block';
   // hide image aria
   imagesArea.style.display = 'none';
-  const duration = document.getElementById('doration').value || 1000;
+  let duration = document.getElementById('doration').value || 1000;
   if (duration > 0) {
     sliders.forEach(slide => {
       let item = document.createElement('div')
@@ -94,12 +94,11 @@ const createSlider = () => {
       changeSlide(slideIndex);
     }, duration);
   }else{
-   alert("Enter a positive value");
+    alert("Enter a positive value");
     document.write("Reload the page");
   }
-    
-
 }
+
 
 // change slider index 
 const changeItem = index => {
@@ -136,7 +135,7 @@ searchBtn.addEventListener('click', function () {
 })
 
 sliderBtn.addEventListener('click', function () {
-  createSlider()
+  createSlider();
 })
 
 // toggle Spinner
@@ -144,3 +143,26 @@ const toggleSpinner = () =>{
   const spinner = document.getElementById('loading-spinner');
   spinner.classList.toggle('d-none');
 }
+
+// clock
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
